1888f821fffab1255256755c42c8bbb6ce3ae55b
KMACREATOR <148497147+KMACREATOR@users.noreply.github.com>
2023-11-19 21:20:56 +0300

    Update калькулятор дз.cpp

--- калькулятор дз.cpp
+++ калькулятор дз.cpp
@@ -5,45 +5,42 @@ using namespace std;
 
 string expression;
 string numbers("0123456789,"); //набор цифр для проверки
-string ops("^*/+-");
-string d;
-//float a, b, c; // переменные для записи членов арифмитических выражений и их результатов
+string ops("^*/+-"); // набор операторов для подстановки
+string d; // строка для записи полученного значения вместо исходного выражения в строку
 int p1, p2; //первый и второй индекс вхождения подвыражения в выражения
-//, int p1, int p2
-//, p1, p2
 void calculator(string expression){
     string temp_expression = expression;
-    while((expression.find("^") != expression.npos) || (expression.find("*") != expression.npos) || (expression.find("/") != expression.npos) || (expression.find("+") != expression.npos) || (expression.find("-") != expression.npos)){
-        int k1 = 0;
-        int k2 = 0; //счетчики для открытых и закрытых скобок
+    while(((expression.find("^") != expression.npos) || (expression.find("*") != expression.npos) || (expression.find("/") != expression.npos) || (expression.find("+") != expression.npos) || (expression.find("-") != expression.npos)) && (expression.rfind("-") != 0)){
+        int k1 = 0, k2 = 0; //счетчики для открытых и закрытых скобок
         if (temp_expression.find("(") != expression.npos){ //в этом куске проверяем выражение на скобки
             for (int i = 0; i < temp_expression.length(); ++i){
                 if (temp_expression[i] == '('){
                     k1 = k1 + 1;
+//                    cout << "количество открытых скобок: " << k1 << endl;
                 }
                 if (temp_expression[i] == ')'){
                     k2 = k2 + 1;
+//                    cout << "количество закрытых скобок: " << k2 << endl;
                 }
-                if (k1 == k2){
-                    p1 = temp_expression.find("(") + 1; //берем выражение уже без скобок
+                if ((k1 == k2) && (k1 != 0)){
+//                    cout << "количество скобок сошлось." << endl;
+                    p1 = temp_expression.find("(")+1; //берем выражение уже без скобок
                     p2 = i - 1;
+//                    cout << "индексы подстроки: " << p1 << " " << p2 << endl;
                     break;
                 }
             }
             temp_expression = temp_expression.substr(p1, p2 - p1 + 1);
             calculator(temp_expression);
         }
-        else{ //здесь обрабатываем выражения без скобок
-            //(уже сделано)нужно добавить проверку на отрицательные числа в начале строки
-            // if ((i != 0) && (i == "-"));
-            cout << "обрабатываемое выражение: " << temp_expression << endl;
-            int g1, g2; //выделение подстрок для работы с операторами
+        else{
+//            cout << "обрабатываемое выражение: " << temp_expression << endl;
             for (int f = 0; f < ops.length(); ++f){
-                cout << "оператор: " << ops[f] << endl;
-                int flag = 0;
+//                cout << "оператор: " << ops[f] << endl;
                 while (temp_expression.find(ops[f]) != temp_expression.npos){ // здесь ищем числа, окружающие нужный нам оператор
-                                    flag = 0;
-                                    int a = 0, b = 0;
+                                    int flag = 0;
+                                    float a = 0, b = 0;
+                                    int g2 = -1;
                                     string n("");
                                     string m(""); //строки для записи членов
                                     cout << "длина: " << temp_expression.length() << endl;
@@ -51,43 +48,39 @@ void calculator(string expression){
                                             cout << "i: " << i << endl;
                                             cout << "флаг: " << flag << endl;
                                             cout << "положение каретки: " << temp_expression[i] << endl;
-                                            if (flag == 1){
-                                                flag = 2;
-                                                cout << "флаг: " << flag << endl;
-                                            }
                                             if (flag == 0){
                                                 if ((numbers.find(temp_expression[i]) != numbers.npos) || ((i == 0) && (temp_expression[i] == '-')) ){
                                                     n = n.append(1, temp_expression[i]);
                                                     cout << "n: " << n << endl;
                                                     }
-                                                if ((ops.find(temp_expression[i]) != ops.npos) && (temp_expression[i] != ops[f])){
-                                                    n = "1";
+                                                if ((ops.find(temp_expression[i]) != ops.npos) && (temp_expression[i] != ops[f]) && (i != 0)){
+                                                    n = "";
                                                     cout << "n очищен" << endl;
                                                     }
-                                                if (temp_expression[i] == ops[f]){
+                                                if ((temp_expression[i] == ops[f]) && (i != 0)){
                                                     flag = 1;
-                                                    a = stoi(n);
+                                                    a = stof(n);
+                                                    g2 = i;
                                                     cout << "сохраненный n: " << n << endl;
+                                                    cout << "индекс начала n:" << e1 << endl;
                                                     }
                                             }
-                                            if (flag == 2){
+                                            if ((flag == 1) && (i != g2)){
                                                 if (i == (temp_expression.length() - 1)){
                                                                 m = m.append(1, temp_expression[i]);
-                                                                b = stoi(m);
+                                                                b = stof(m);
                                                                 cout << "сохраненный m: " << m << endl;
-                                                                g2 = i;
                                                             }
                                                 else{
                                                     if (numbers.find(temp_expression[i]) != numbers.npos){
                                                             m = m.append(1, temp_expression[i]);
-                                                            cout << m << endl;
-
+                                                            cout << "m: " << m << endl;
                                                     }
                                                     if ((ops.find(temp_expression[i]) != ops.npos)){
-                                                        b = stoi(m);
-                                                        cout << m << endl;
-                                                        g2 = i;
+                                                        b = stof(m);
+                                                        cout << "сохраненный m: " << m << endl;
                                                         break;
+                                                    break;
                                                         }
 
                                             }
@@ -123,15 +116,13 @@ void calculator(string expression){
                 a = 0;
                 b = 0;
                 c = 0;
-                expression.replace(0, g2+1, d);
+                expression.replace(g2 - n.length(), n.length() + m.length() + 1, d);
                 cout << "выражение после замены: " << expression << endl;
                 temp_expression = expression;
                 }
 
                     }
             }
-
-
         }
         cout << expression << endl;
 }
@@ -151,7 +142,5 @@ int main()
 }
 
 
-//    int t; // переменная приоритета операторов
-//                            a = 0;
-//                            b = 0;
-//                            c = 0;
+
+//дз2 Калькулятор Конов Михаил
